a727162520a3f92d45882751a75fb303d2eb211d
Dylan Travers <dylantravers777@gmail.com>
2023-12-09 18:35:39 +0800

    day 1 - part 1 and 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,11 +1,34 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var total = 0
+        for (line in input) {
+            val digits = line.filter{ it.isDigit() }
+            if (digits.isNotEmpty()){
+                val sum: String = digits.first().toString() + digits.last().toString()
+                total += sum.toInt()
+            }
+        }
+        return total
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>) : Int {
+
+        val dict = mapOf("one" to 1, "two" to 2, "three" to 3, "four" to 4, "five" to 5, "six" to 6, "seven" to 7, "eight" to 8, "nine" to 9)
+        val possibleValues = dict.keys + dict.values.map { it.toString() }
+
+        var total = 0
+        for (line in input) {
+            val firstNumber = line.findAnyOf(possibleValues, 0,true)?.second.toString()
+            val secondNumber = line.findLastAnyOf(possibleValues, line.length,true)?.second.toString()
+
+            val first: String = if(firstNumber.length > 1) dict[firstNumber].toString() else firstNumber
+            val second: String = if (secondNumber.length > 1) dict[secondNumber].toString() else secondNumber
+
+            total += "$first$second".toInt()
+        }
+        return total
     }
+
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
